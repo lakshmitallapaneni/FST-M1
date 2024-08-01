14dfcac5e91acdd3d27d0a67153617166c0b1997
lakshmitallapaneni <lakshmi12km@gmail.com>
2024-08-01 17:05:41 +0530

    Add files via upload

--- TestNG/Activities/Activity6.java
+++ TestNG/Activities/Activity6.java
@@ -1,4 +1,15 @@
-package TestNG;
+package Activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
 
 public class Activity6 {
     WebDriver driver;
@@ -7,11 +18,11 @@ public class Activity6 {
     @BeforeClass
     public void beforeClass() {
         // Set up the Firefox driver
-        WebDriverManager.firefox().setup();
+        WebDriverManager.firefoxdriver().setup();
         // Initialize the Firefox driver
         driver = new FirefoxDriver();
         // Initialize the wait object
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         //Open browser
         driver.get("https://v1.training-support.net/selenium/login-form");
